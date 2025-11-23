6a51551bf1f33da262ea3b2b9bcf421705d008b7
henrio randriasoa <rioxdev@gmail.com>
2025-11-23 14:38:56 +0100

    initialization

---
+++ rxapicontainerised/Controllers/DemoController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace rxapicontainerised.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DemoController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello from DemoController!");
+        }
+    }
+}
